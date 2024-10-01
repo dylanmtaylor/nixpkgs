a989f1f59500d2d56850a529cd916b8a3db0366c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-10-01 09:19:35 +0200

    jbigkit: add patches
    Also fix security issue CVE-2017-9937

--- pkgs/development/libraries/jbigkit/default.nix
+++ pkgs/development/libraries/jbigkit/default.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchurl,
+  fetchpatch,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -13,9 +14,42 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-3nEGtr+vSV1oZcfdesbKE4G9EuDYFAXqgefyFnJj2TI=";
   };
 
+  patches = [
+    # Archlinux patch: build shared object
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/jbigkit-2.1-shared_lib.patch?ref_type=heads";
+      hash = "sha256-+efeeKg3FJ/TjSOj58kD+DwnaCm3zhGzKLfUes/d5rg=";
+    })
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/jbigkit-2.1-ldflags.patch?ref_type=heads";
+      hash = "sha256-ik3NifyuhDHnIMTrNLAKInPgu2F5u6Gvk9daqrn8ZhY=";
+    })
+    # Archlinux patch: update coverity
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/jbigkit-2.1-coverity.patch?ref_type=heads";
+      hash = "sha256-APm9A2f4sMufuY3cnL9HOcSCa9ov3pyzgQTTKLd49/E=";
+    })
+    # Archlinux patch: fix build warnings
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/jbigkit-2.1-build_warnings.patch?ref_type=heads";
+      hash = "sha256-lDEJ1bvZ+zR7K4CiTq+aXJ8PGjILE3W13kznLLlGOOg=";
+    })
+    # Archlinux patch: this helps users to reduce denial-of-service risks, as in CVE-2017-9937
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/0013-new-jbig.c-limit-s-maxmem-maximum-decoded-image-size.patch?ref_type=heads";
+      hash = "sha256-Yq5qCTF7KZTrm4oeWbpctb+QLt3shJUGEReZvd0ey9k=";
+    })
+    # Archlinux patch: fix heap overflow
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/0015-jbg_newlen-check-for-end-of-file-within-MARKER_NEWLE.patch?ref_type=heads";
+      hash = "sha256-F3qA/btR9D9NfzrNY76X4Z6vG6NrisI36SjCDjS+F5s=";
+    })
+  ];
+
   makeFlags = [
     "AR=${lib.getBin stdenv.cc.bintools.bintools}/bin/${stdenv.cc.targetPrefix}ar"
     "CC=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc"
+    "DESTDIR=${placeholder "out"}"
     "RANLIB=${lib.getBin stdenv.cc.bintools.bintools}/bin/${stdenv.cc.targetPrefix}ranlib"
   ];
 
@@ -30,27 +64,25 @@ stdenv.mkDerivation (finalAttrs: {
   installPhase = ''
     runHook preInstall
 
-    install -D -m644 libjbig/libjbig.a $out/lib/libjbig.a
-    install -D -m644 libjbig/libjbig85.a $out/lib/libjbig85.a
-    install -D -m644 libjbig/jbig.h $out/include/jbig.h
-    install -D -m644 libjbig/jbig_ar.h $out/include/jbig_ar.h
-    install -D -m644 libjbig/jbig85.h $out/include/jbig85.h
+    install -vDm 644 libjbig/*.h -t "$out/include/"
+    install -vDm 755 pbmtools/{jbgtopbm{,85},pbmtojbg{,85}} -t "$out/bin/"
+    install -vDm 644 pbmtools/*.1* -t "$out/share/man/man1/"
 
-    install -d -m755 $out/share/man/man1
-    install -m644 pbmtools/*.1* $out/share/man/man1
-
-    install -D -m755 pbmtools/jbgtopbm $out/bin/jbgtopbm
-    install -D -m755 pbmtools/pbmtojbg $out/bin/pbmtojbg
-    install -D -m755 pbmtools/jbgtopbm85 $out/bin/jbgtopbm85
-    install -D -m755 pbmtools/pbmtojbg85 $out/bin/pbmtojbg85
+    install -vDm 755 libjbig/*.so.* -t "$out/lib/"
+    for lib in libjbig.so libjbig85.so; do
+      ln -sv "$lib.${finalAttrs.version}" "$out/lib/$lib"
+      ln -sv "$out/lib/$lib.${finalAttrs.version}" "$out/lib/$lib.0"
+    done
 
     runHook postInstall
   '';
+
+  doCheck = true;
 
   meta = {
     description = "Software implementation of the JBIG1 data compression standard";
     homepage = "http://www.cl.cam.ac.uk/~mgk25/jbigkit/";
     license = lib.licenses.gpl2Plus;
-    platforms = lib.platforms.all;
+    platforms = lib.platforms.linux;
   };
 })
